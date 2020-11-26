b282d85bf200b5f792eb0db1a73ab7d03120d745
GonzalezGabriel-dev <71902209+GonzalezGabriel-dev@users.noreply.github.com>
2020-11-25 20:38:39 -0700

    Funciones Predefinidas

---
+++ funcional/introduccion/funcionespredefinidas.hs
@@ -0,0 +1,30 @@
+--devuelve el valor absoluto de x
+funcion1 a = abs a
+--menor enterno no menor que x 
+funcion2 a = ceiling a
+--devuelve el valor siguiente de x
+funcion3 a = succ a
+--obtiene el menor
+funcion4 a b = min a b 
+--obtiene el mayor
+funcion5 a b = max a b
+--divide x entre y
+funcion6 a b = div a b
+--obtiene el modulo de x y
+funcion7 a b = mod a b
+--verifica si x es par
+funcion8 a = even a
+--numero mayor entero no  mayor que x
+funcion9 a = floor a
+--máximo común divisor
+funcion10 a b = gcd a b
+--negación de booleana
+funcion11 a = not a
+--verifica si x es numero impar
+funcion12 a = odd a
+--devuelve el valor siguiente de x
+funcion13 a = succ a
+--devuelve 1 si es positivo, -1 si es negativo
+funcion14 a = signum a
+--retorna el segundo elemento. Y en este caso
+funcion15 a b = snd(a,b)
